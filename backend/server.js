95a280f327eae75ff343b0b0830109f2fe6eb684
Emilia <emimiqq@gmail.com>
2022-04-28 11:57:41 -0400

    Update backend port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 30081;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/auth"));
