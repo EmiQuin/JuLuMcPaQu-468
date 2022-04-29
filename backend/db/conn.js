a55b81d8d0959b984c0d14b91dc282de12593139
Devin Parentice <35779219+DevinParentice@users.noreply.github.com>
2022-04-29 14:29:33 -0400

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+	console.log(`Process: ${process.env.ATLAS_URI}`)
 	await mongoose.connect(process.env.ATLAS_URI, {
 		dbName: "ChessGame",
 		useNewUrlParser: true,
