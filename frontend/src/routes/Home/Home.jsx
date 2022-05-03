1d2282d14809ad97cb85ffc5097b57a429f587db
bam1098 <bmcfalls1@gmail.com>
2022-05-03 19:33:46 -0400

    Update Home.jsx

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -10,6 +10,7 @@ function Home() {
 			<Text>
 				Edit this page at: <code>frontend/routes/Home/Home.jsx</code>
 			</Text>
+			<img src="https://images.ctfassets.net/3s5io6mnxfqz/wfAz3zUBbrcf1eSMLZi8u/c03ac28c778813bd72373644ee8b8b02/AdobeStock_364059453.jpeg?fm=jpg&w=900&fl=progressive" alt="chess background">
 		</div>
 	);
 }
