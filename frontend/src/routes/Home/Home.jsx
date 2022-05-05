79fd79991b1299e0a6a19036a5c133a95a633247
bam1098 <bmcfalls1@gmail.com>
2022-05-04 20:22:51 -0400

    Update Home.jsx

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -8,8 +8,9 @@ function Home() {
 				Cloud Chess
 			</Text>
 			<Text>
-					Sign in and play through the menu on the left
+				Sign in and play through the menu on the left
 			</Text>
+			<img src="https://images.ctfassets.net/3s5io6mnxfqz/wfAz3zUBbrcf1eSMLZi8u/c03ac28c778813bd72373644ee8b8b02/AdobeStock_364059453.jpeg?fm=jpg&w=900&fl=progressive" alt="chess photo"
 		</div>
 	);
 }
